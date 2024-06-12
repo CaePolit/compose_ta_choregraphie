371c1e7c31afe8a5c25bcba4baca25ba8a506991
CaePolit <caepolit@gmail.com>
2024-06-12 11:28:55 +0200

    Ajoutez des fichiers projet.

---
+++ compose_ta_choregraphie/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        string[] moves = new string[32];
+        int l = 0;
+        while (l < 32)
+        {
+            Console.WriteLine("insert move " + l);
+            string move = Console.ReadLine();
+            if (move == "done")
+            {
+                break;
+            }
+            else
+            {
+                moves[l] = move;
+                l++;
+            }
+        }
+        Console.WriteLine("number of iterations: ");
+        int iterations = default;
+        string userEntry = default;
+
+        while (true)
+        {
+            userEntry = Console.ReadLine();
+            if (int.TryParse(userEntry, out iterations))
+            {
+                if (iterations > 0)
+                    break;
+                else
+                    Console.WriteLine("invalid input");
+            }
+        }
+
+        int i = 0;
+        while (i < iterations)
+        {
+            foreach (string move in moves)
+            {
+                Console.WriteLine(move);
+            }
+            i++;
+        }
+
+    }
+}
